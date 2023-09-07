226d96cc02428a0cbb333aaa0aba732417619686
Motebang <motebangmokwatsi0@gmail.com>
2023-09-07 20:04:36 +0200

    feat: Home page used to call the ciew models method getCharacters which will return the data of the character and we display the list on the home page

---
+++ question_3/lib/views/screens/home.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:question_3/utils/loading.dart';
+import 'package:question_3/views/viewmodels/character_viewmodel.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_conditional_rendering/conditional_switch.dart';
+import 'package:question_3/views/widgets/character_list.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      Provider.of<CharacterViewmodel>(context, listen: false).getCharacters();
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Rick and Morty Characters'),
+      ),
+      body: Consumer<CharacterViewmodel>(
+        builder: (BuildContext context, CharacterViewmodel viewmodel, _) {
+          return _checkStatus(viewmodel);
+        },
+      ),
+    );
+  }
+
+  Widget _checkStatus(CharacterViewmodel viewmodel) {
+    return ConditionalSwitch.single<LoadingStatus>(
+      context: context,
+      valueBuilder: (BuildContext context) => viewmodel.fetchState,
+      caseBuilders: {
+        LoadingStatus.fetching: (BuildContext context) =>
+            const Center(child: CircularProgressIndicator()),
+        LoadingStatus.notfetched: (BuildContext context) =>
+            const Text("could not retrieve the characters"),
+        LoadingStatus.done: (BuildContext context) =>
+            _createContent(viewModel: viewmodel),
+        LoadingStatus.error: (BuildContext context) =>
+            const Text("an error occured"),
+      },
+      fallbackBuilder: (BuildContext context) => const SizedBox(),
+    );
+  }
+
+  _createContent({required CharacterViewmodel viewModel}) {
+    return CharacterListView(
+      characterList: viewModel.character!.results!,
+      info: viewModel.character!.info!,
+    );
+  }
+}
